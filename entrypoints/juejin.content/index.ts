8dae0f383511aa4945217cdd44a9402450459d89
Deng <aianddeng@gmail.com>
2025-09-30 18:03:50 +0800

    feat: init project

---
+++ entrypoints/juejin.content/index.ts
@@ -0,0 +1,20 @@
+import "./style.css"
+
+export default defineContentScript({
+  matches: ["*://*.juejin.cn/*"],
+  runAt: "document_end",
+  main(ctx) {
+    const ui = createIntegratedUi(ctx, {
+      position: "inline",
+      anchor: "body",
+      onMount: (container) => {
+        const cover_div = document.createElement("div")
+        cover_div.style =
+          "position: fixed; inset: 0; z-index: 1000000000; background: rgba(0,0,0,.5); pointer-events: none;"
+        container.append(cover_div)
+      },
+    })
+
+    ui.mount()
+  },
+})
