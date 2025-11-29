7115b15f3188a70ae59245b0e72a8c6cae27b172
Deng <aianddeng@gmail.com>
2025-11-29 14:16:40 +0800

    feat: extension key

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,6 +1,9 @@
-import { defineConfig } from 'wxt';
+import { defineConfig } from "wxt"
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  modules: ['@wxt-dev/module-react'],
-});
+  modules: ["@wxt-dev/module-react"],
+  manifest: () => ({
+    key: "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAuw4VcQYZ+VuTEPhB6JONuMGMnuDMatkbbkJogJ3AJOteNmsDcgX7u/HOGFJhr8DgxBvkGnjki0jTS5DrNEPeFpcxd0VKVijXJ3HVtMX0jK9crqHhHBal7jAzhTc66sbOvmBRx1NnnGaJtQk/R/iGjHaI/f9/Ag//QMiHtqvpCc+FqX1PPDju/JkOEif1y6llNpY8v92pdP9eyhU4Pf9rf3xykvHmM+ykSvAj+1vApyOzuZByTBImVvB57CA3HjGgcgXDCbTAD8snvMpD8HE7rlDNH4IJ8Do0ijQ8ZlTcTYEoDNSV/YMA+Sg6rU8c9tNQ72HyLolYN0+xQbbXzMBp/QIDAQAB",
+  }),
+})
